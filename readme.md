13bc582c37315a97ddc64c2c1ce648ca3b24ebba
Felipe Morales <felipe.morales.querol@gmail.com>
2021-09-19 23:42:12 -0300

    Fix de test case

--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 # Boilerplate
 `npm init -y`
 
-`npm i express morgan`
+`npm i express morgan mocha`
+
+`npm i node-fetch@2.6.2 chai`
---
+++ test/test-api-auth.js
@@ -0,0 +1,31 @@
+const assert = require('chai').assert;
+const fetch = require('node-fetch')
+
+describe("Probando API Auth", () => {
+
+    it("API responde 200", async () => {
+        await fetch('http://localhost:3000/api/me')
+            .then(response => {
+                console.log(response.status)
+                assert.equal(response.status, 200);
+            })
+    });
+
+    // it("API include key status", async () => {
+    //     await fetch('http://localhost:3000/api/me')
+    //         .then(response => response.json())
+    //         .then(json => {
+    //             expect(json).includes("status", "Status");
+    //         })
+    // });
+
+    it("API include text me", async () => {
+        await fetch('http://localhost:3000/api/me')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                assert.deepEqual(json, { "status": "me" });
+            })
+    });
+
+});
