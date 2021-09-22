139df94b2f7498a0654208b1b80890656097afc4
Felipe Morales <felipe.morales.querol@gmail.com>
2021-09-21 21:15:37 -0300

    Agregar funcion dividir en utils y testing sobre openweathermap

--- api/utils/utils.js
+++ api/utils/utils.js
@@ -1,9 +1,14 @@
 function suma(a, b) {
-    return a + b
+    const resultado = a + b;
+    return resultado
 };
 
 function resta(a, b) {
     return a - b
 };
 
-module.exports = { suma, resta };
+function dividir(a, b) {
+    return a / B
+};
+
+module.exports = { suma, resta, dividir };
--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
-const { suma, resta } = require('../api/utils/utils')
+const { suma, resta, dividir } = require('../api/utils/utils')
 
 var assert = require('assert');
-describe('suma', function () {
+describe('suma, resta y división', function () {
+
+
     describe('#suma', function () {
         it('Suma de 2 + 2 es 4', function () {
             assert.equal(suma(2, 2), 4);
@@ -24,5 +26,21 @@ describe('suma', function () {
 
     });
 
+    describe('#división', function () {
+        it('División por si mismo (2 / 2) da 1', function () {
+            assert.equal(dividir(2, 2), 1);
+        });
+
+        it('División por la unidad (4 / 1) da 4', function () {
+            assert.equal(dividir(4, 1), 4);
+        });
+
+        it('División por cero (4 / 0) da Infinity', function () {
+            assert.equal(dividir(4, 0), Infinity);
+        });
+
+
+
+    });
 
 });
